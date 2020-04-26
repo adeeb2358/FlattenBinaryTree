c89b8c067846381f86421d2b0175218886a12e54
Adeeb Mohammed <adeeb2358@gmail.com>
2020-04-27 01:49:12 +0900

    flattening a binary tree non optimised version

--- FlattenBinaryTree/main.cpp
+++ FlattenBinaryTree/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
+#include <vector>
+
+
+// This is the class of the input root. Do not edit it.
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left = NULL;
+  BinaryTree *right = NULL;
+  BinaryTree(int value);
+};
+
+
+// non optimized method
+// function for getting the non optimized nodes.
+void getNodesInOrder(BinaryTree *node,
+              std::vector<BinaryTree*> &nodes){
+    if(node != NULL){
+        getNodesInOrder(node->left, nodes);
+        nodes.push_back(node);
+        getNodesInOrder(node->right, nodes);
+    }
+}
+
+BinaryTree *flattenBinaryTree(BinaryTree *root) {
+    std::vector<BinaryTree*> nodes;
+    getNodesInOrder(root, nodes);
+    for(int i = 0 ; i < nodes.size()-1; i++){
+        BinaryTree *leftNode = nodes.at(i);
+        BinaryTree *rightNode = nodes.at(i+1);
+        leftNode->right = rightNode;
+        rightNode->left = leftNode;
+    }
+    return nodes.at(0);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Flatten Binary Tree. Inorder traversal\n";
     return 0;
 }
